48902cbf1c6749be6a4053575a8050bc64ab925c
Santiago Fleitas <sansfleitas@gmail.com>
2023-10-08 15:24:38 -0300

    Create user example in Prisma

--- prisma/index.ts
+++ prisma/index.ts
@@ -2,7 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Santiago',
+      email: 'san@san.com',
+      password: 'Q1Q2121VAS',
+      created_at: new Date(),
+    },
+  });
+}
 
 main()
   .then(async () => {
