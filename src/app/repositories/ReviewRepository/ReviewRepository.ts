429ff501706bcb622ba9999981492264eba260d8
Santiago Fleitas <sansfleitas@gmail.com>
2023-11-13 02:41:59 -0300

    Add information in the review response

--- src/app/repositories/ReviewRepository/ReviewRepository.ts
+++ src/app/repositories/ReviewRepository/ReviewRepository.ts
@@ -3,16 +3,26 @@ import { CreateReviewInput } from './types';
 
 export class ReviewRepository {
   async save({ description, gameId, rating, userId }: CreateReviewInput) {
-    // Vai ter que criar com o id_igdb, pois pode ser que o game em questão não tenha sido criado ainda por nenhuma lista
-    console.log('Dados que chegam no repository');
-    console.log({ description, gameId, rating, userId })
-
     const createdReview = await prisma.userRating.create({
       data: {
         description,
         gameId,
         rating,
         userId,
+      },
+      select: {
+        id: true,
+        description: true,
+        rating: true,
+        gameId: true,
+        userId: true,
+        createdAt: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+          }
+        }
       }
     });
 
