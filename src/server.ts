054d74431ee0402778c9f4e31b31921a6e78c910
Santiago <sansfleitas@gmail.com>
2023-11-20 21:15:08 -0300

    Config CORS to production

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 
@@ -9,16 +9,30 @@ import listsRoute from './app/routes/listsRoute';
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV !== 'development') {
+  // Configurações para produção
+  const corsOptions = {
+    origin: process.env.FRONTEND_IP,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  app.use(cors(corsOptions));
+}
+
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = Number(process.env.APP_PORT) || 5765
+const port = Number(process.env.APP_PORT) || 5765;
 
 app.use('/api/games', gamesRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/lists', listsRoute);
 
 app.listen(port, () => {
-  console.log(`🚀 Application is running at port: ${port}`)
+  console.log(`🚀 Application is running at port: ${port}`);
 });
