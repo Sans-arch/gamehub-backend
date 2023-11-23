d64ff753293f8352de6a4d7512f5c8894dbfb5aa
Santiago Fleitas <sansfleitas@gmail.com>
2023-11-22 23:22:43 -0300

    Fix CORS

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ import listsRoute from './app/routes/listsRoute';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
