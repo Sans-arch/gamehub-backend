32e29c07a59f61692a3e984967b0a42cf01bf1e2
Santiago <sansfleitas@gmail.com>
2023-11-24 21:12:51 -0300

    Create variables to test

--- src/tests/services/gamesService.ts
+++ src/tests/services/gamesService.ts
@@ -1,10 +1,20 @@
-import gamesService from '../../app/services/gamesService';
+import { GameRepository } from '../../app/repositories/GameRepository/types';
+import { ReviewRepository } from '../../app/repositories/ReviewRepository/ReviewRepository';
+import { GameService } from '../../app/services/gamesService';
 
 describe('GameService test suite', () => {
+  let mockGameRepository: GameRepository;
+  let mockReviewRepository: ReviewRepository;
+  let mockGamesService;
+
+  beforeEach(() => {
+    mockGamesService = new GameService(mockGameRepository, mockReviewRepository);
+  });
+
   it('should return a list of getGamesById', async () => {
     const gamesIds = ['1020', '1942'];
 
-    const games = await gamesService.getGamesById(gamesIds);
+    const games = await mockGamesService.getGamesById(gamesIds);
 
     expect(games).not.toBeUndefined();
     expect(games).toHaveLength(2);
@@ -12,6 +22,6 @@ describe('GameService test suite', () => {
 
   it('should return a game info by slug', async () => {
     const slug = 'the-last-of-us-part-ii';
-    const gameInfo = await gamesService.getGameInfo(slug);
+    const gameInfo = await mockGamesService.getGameInfo(slug);
   });
 });
