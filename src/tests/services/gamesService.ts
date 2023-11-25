422d2dc24480e1d1288e3ee2cdda239692e83982
Santiago <sansfleitas@gmail.com>
2023-11-24 21:04:10 -0300

    Refactor GameService to class to ease the dependency injection

--- src/tests/services/gamesService.ts
+++ src/tests/services/gamesService.ts
@@ -1,12 +1,19 @@
 import gamesService from '../../app/services/gamesService';
 
 describe('GameService test suite', () => {
-  it('should return a list of games', async () => {
+  it('should return a list of getGamesById', async () => {
     const gamesIds = ['1020', '1942'];
 
     const games = await gamesService.getGamesById(gamesIds);
 
     expect(games).not.toBeUndefined();
     expect(games).toHaveLength(2);
+  });
+
+  it('should return a game info by slug', async () => {
+    const slug = 'the-last-of-us-part-ii';
+    const gameInfo = await gamesService.getGameInfo(slug);
+
+    console.log(gameInfo);
   });
 });
